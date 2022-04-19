9058c4c020c229166ebb2f854dccf1b517a13d32
Siming Deng <siming.deng@baruchmail.cuny.edu>
2022-04-19 22:24:22 +0000

    created five models (dim_customers, fact_orders, stg_customers, stg_orders, and stg_payments)

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,12 @@
+with payments as (
+    
+    select id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        amount/100 as amount,
+        created as created_date,
+    from `dbt-tutorial`.stripe.payment
+)
+
+select * from payments
