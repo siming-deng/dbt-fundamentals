6f1ac2df46805bc9cf693fa25bc99e3a7b161ace
Siming Deng <siming.deng@baruchmail.cuny.edu>
2022-04-20 21:45:40 +0000

    dbt run

--- models/customers_daily_summary.sql
+++ models/customers_daily_summary.sql
@@ -2,6 +2,6 @@ select
     {{ dbt_utils.surrogate_key(['customer_id', 'order_date'])}} as id,
     customer_id,
     order_date,
-    count(*)
+    count(*) as number_of_records
 from {{ ref('stg_orders') }}
 group by 1,2,3
