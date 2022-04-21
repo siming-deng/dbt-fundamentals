58e0f45f912c514635f6522e5d63624e566743f5
Siming Deng <siming.deng@baruchmail.cuny.edu>
2022-04-21 03:05:21 +0000

    created an analysis file in the analyses folder called total_revenue.sql that uses the stg_payments model and sums the amount of successful payments. Created a seed file in the seeds folder called employees.csv with the values added and ran the dbt seed command

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -12,4 +12,4 @@ with orders as (
 
 select * from orders
 
---{{ limit_data_in_dev(column_name = 'order_date', dev_days_of_data = 1500) }}
+{#  {{ limit_data_in_dev(column_name = 'order_date', dev_days_of_data = 1500) }} #}
